c2b5f41337f9177f6d622ebbf4e105877270695a
mabubakar <mabubakar6464@gmail.com>
2026-01-01 19:52:26 +0500

    updated login command session keys

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,7 +4,7 @@ Cypress.Commands.add('loginSession', () => {
     const username = Cypress.env('validUsername');
     const password = Cypress.env('validPassword');
 
-    cy.session('', () => {
+    cy.session([username, password], () => {
         loginPage.visit();
         loginPage.login(username, password);
         cy.url({ timeout: 10000 }).should('include', '/assignments');
